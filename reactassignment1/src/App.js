b2d33754fdce62d898b1b6db1487070a14e5b62c
Neha Sontakke <wasnik.neha@gmail.com>
2022-01-11 17:11:24 +0530

    added the assignment

---
+++ reactassignment1/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Sign from "./components/Sign";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div className="outer-container">
+      <div className="inner-container">
+        <Sign/>
+      </div>
+    </div>
+  </div>
+  );
+}
+
+export default App;
